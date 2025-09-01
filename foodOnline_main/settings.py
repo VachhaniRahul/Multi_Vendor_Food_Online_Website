34bec1d2f8832f673d7c745a355a722f76d191f4
Rahul patel <vachhanirahul54@gmail.com>
2025-09-01 19:42:13 +0530

    add: password reset and email varification

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
-
 from django.contrib.messages import constants as messages
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,8 +145,6 @@ DATABASES = {
         "PORT": config("DB_PORT", default="5432"),
     }
 }
-
-
 
 
 MESSAGE_TAGS = {
